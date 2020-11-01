5017806e1df31806f971f1a4ac0e9d0d5b333568
Godohr <aheneoaqu@gmail.com>
2020-11-01 17:36:12 +0900

    g

--- hello.c
+++ hello.c
@@ -1,13 +1,2 @@
-
-# Please enter the commit message for your changes. Lines starting
-# with '#' will be ignored, and an empty message aborts the commit.
-#
-# On branch master
-# Changes to be committed:
-#	modified:   hello.c
-#
-# Untracked files:
-#	add
-#	commit
-#
-hi
+#include <stdio.h>
+int main
